65564999ee642037c7d2c06ab6cf8725e67a5f78
PetranTinak <anonie@azet.sk>
2017-02-28 14:10:23 +0100

    Zadanie 1

--- OpenCvTest/Source.cpp
+++ OpenCvTest/Source.cpp
@@ -1,54 +1,9 @@
 #include "opencv2/opencv.hpp"
-#include <sstream>
 #include <iostream>
 
-
-using namespace cv;
 using namespace std;
-
-Mat frame, img;
+using namespace cv;
 int counter;
-
-int main(int, char**)
-{
-	
-	while (counter>50)
-	{
-		stringstream file;
-
-		VideoCapture cap(0); // open the default camera
-		if (!cap.isOpened())  // check if we succeeded
-			return -1;
-
-		Mat edges;
-		namedWindow("edges", 1);
-
-		Mat frame;
-		cap >> frame; // get a new frame from camera
-
-		//if (frame.empty()) break;
-		file << "C:/Users/Boi/Documents/Obrazky/image" << counter << ".bmp";
-		counter++;
-		imwrite(file.str(), frame);
-
-		char key = waitKey(10);
-		if (key == 27)
-		{
-			break;
-		}
-
-
-	}
-
-
-}
-
-/*
-#include "opencv2/opencv.hpp"
-#include <iostream>
-
-using namespace std;
-using namespace cv;
 
 int main() {
 
@@ -62,15 +17,29 @@ int main() {
 	int frame_height = vcap.get(CV_CAP_PROP_FRAME_HEIGHT);
 	VideoWriter video("out.avi", CV_FOURCC('M', 'J', 'P', 'G'), 10, Size(frame_width, frame_height), true);
 
-	for (;;) {
+	while (counter<100) {
 
 		Mat frame;
+		stringstream file;
 		vcap >> frame;
-		video.write(frame);
-		imshow("Frame", frame);
+		file << "C:/Users/Boi/Documents/Obrazky/image" << counter << ".bmp";
+		counter++;
+		imwrite(file.str(), frame);
 		char c = (char)waitKey(33);
 		if (c == 27) break;
 	}
+	int i = 0;
+
+	while (counter != i) {
+		Mat image;
+		string fajl;
+		std::string s = std::to_string(i);
+		fajl = "C:/Users/Boi/Documents/Obrazky/image" + s + ".bmp";
+		image = imread(fajl, CV_LOAD_IMAGE_UNCHANGED);
+		video.write(image);
+		imshow("Frame", image);
+		i++;
+	}
+
 	return 0;
 }
-*/
